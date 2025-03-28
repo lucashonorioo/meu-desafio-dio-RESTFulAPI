349dcb071c0c755bb25fb87cb64ec26457ce8e46
Lucas Honorio <lucashonorioo@hotmail.com>
2025-03-28 15:16:49 -0300

    Bugfix CORS

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,7 +1,18 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(title = "API Santander Dev Week", version = "1.0", description = "Documentação da API"),
+		servers = {
+				@Server(url = "/", description = "Local"),
+				@Server(url = "meu-desafio-dio-restfulapi-production.up.railway.app", description = "Railway")
+		}
+)
 
 @SpringBootApplication
 public class Application {
